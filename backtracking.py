99c3fdb9b0036aef7719f78a4160dd9485e1dc49
maykonmarcosjunior <maykon.marcos.junior.@grad.ufsc.br>
2023-10-02 19:32:37 -0300

    Incluindo o algoritmo para o sudoku normal

---
+++ backtracking.py
@@ -0,0 +1,100 @@
+'''
+Algorithm
+Starting with an incomplete board:
+
+1. Find some empty space
+2. Attempt to place the digits 1-9 in that space
+3. Check if that digit is valid in the current
+spot based on the current board
+4. a. If the digit is valid, recursively attempt
+to fill the board using steps 1-3.
+4. b. If it is not valid, reset the square you
+just filled and go back to the previous step.
+5. Once the board is full by the definition of
+this algorithm we have found a solution.
+'''
+
+
+# encontra a primeira célula vazia
+def find_empty(board):
+    for row in range(len(board)):
+        for col in range(len(board[0])):
+            if board[row][col] == 0:
+                return (row, col)
+
+    return None
+
+
+def solve(board):
+    find = find_empty(board)
+    if not find:
+        return True
+    else:
+        row, col = find
+
+    for num in range(1, 10):
+        if valid(board, num, row, col):
+            board[row][col] = num
+
+            if solve(board):
+                return True
+
+            board[row][col] = 0
+
+    return False
+
+
+def valid(board, num, row, col):
+    # Check row
+    for i in range(len(board[0])):
+        if board[row][i] == num and col != i:
+            return False
+
+    # Check column
+    for i in range(len(board)):
+        if board[i][col] == num and row != i:
+            return False
+
+    # Check box
+    box_x = col // 3
+    box_y = row // 3
+
+    for i in range(box_y*3, box_y*3 + 3):
+        for j in range(box_x * 3, box_x*3 + 3):
+            if board[i][j] == num and (i != row or j != col):
+                return False
+
+    return True
+
+
+def print_board(bo):
+    for i in range(len(bo)):
+        if i % 3 == 0 and i != 0:
+            print("- - - - - - - - - - - - - ")
+
+        for j in range(len(bo[0])):
+            if j % 3 == 0 and j != 0:
+                print(" | ", end="")
+
+            if j == 8:
+                print(bo[i][j])
+            else:
+                print(str(bo[i][j]) + " ", end="")
+
+
+board = [
+    [7, 8, 0, 4, 0, 0, 1, 2, 0],
+    [6, 0, 0, 0, 7, 5, 0, 0, 9],
+    [0, 0, 0, 6, 0, 1, 0, 7, 8],
+    [0, 0, 7, 0, 4, 0, 2, 6, 0],
+    [0, 0, 1, 0, 5, 0, 9, 3, 0],
+    [9, 0, 4, 0, 6, 0, 0, 0, 5],
+    [0, 7, 0, 3, 0, 0, 0, 1, 2],
+    [1, 2, 0, 0, 0, 7, 4, 0, 0],
+    [0, 4, 9, 2, 0, 6, 0, 0, 7]
+]
+
+print_board(board)
+solve(board)
+print("_______________________\n")
+print_board(board)
