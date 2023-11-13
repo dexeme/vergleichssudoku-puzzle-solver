72ba4e4dfa688f7ede50153b1b6fd044956fe811
maykonmarcosjunior <maykonmjunior@gmail.com>
2023-11-12 22:02:03 -0300

    Versão 0 de debug

--- T2/solver.lisp
+++ T2/solver.lisp
@@ -14,167 +14,144 @@
   (cell-first cell))
 
 (defun get-second (cell)
-  (elt cell 1))
+  (cell-second cell))
 
 (defun get-third (cell)
-  (elt cell 2))
+  (cell-third cell))
 
 (defun get-fourth (cell)
-  (elt cell 3))
+  (cell-fourth cell))
 
 (defun get-fifth (cell)
-  (elt cell 4))
+  (cell-fifth cell))
 
-;; para parametrizar e limpar o código
+;; Parametrização e limpeza do código
 (defun box-size (n)
   (cond ((= n 4) '(2 2))
         ((= n 6) '(3 2))
         ((= n 9) '(3 3))
-        (t (error "Invalid board size"))
-  )
-)
+        (t (error "Invalid board size"))))
 
-;; Verifica se um número não está repetido na linha atual
 (defun isRowValid (board num row)
-  (not (member num (mapcar #'first-elem (nth row (board-cells board)))))
-)
+  ;; Verifica se um número não está repetido na linha atual
+  (not (member num (mapcar #'cell-first (nth row (board-cells board))))))
 
-;; Verifica se um número não está repetido na coluna atual
 (defun isColValid (board num col)
-  (not (member num (mapcar (lambda (cell) (first-elem (nth col cell))) (board-cells board))))
-)
+  ;; Verifica se um número não está repetido na coluna atual
+  (not (member num (mapcar (lambda (cell) (cell-first cell)) 
+                           (mapcar (lambda (row) (nth col row)) 
+                                   (board-cells board))))))
 
-;; Verifica se um número não está repetido na subcaixa atual
 (defun isBoxValid (board num row col)
-  (let* ((box-size (box-size (length (board-cells board))))
-         (box-rows (first box-size))
-         (box-cols (second box-size))
+  ;; Verifica se um número não está repetido na subcaixa atual
+  (let* ((size (box-size (length (board-cells board))))
+         (box-rows (first size))
+         (box-cols (second size))
          (start-row (* (/ row box-rows) box-rows))
          (start-col (* (/ col box-cols) box-cols))
-         (box (loop for r from start-row to (+ start-row box-rows -1)
-                    append (loop for c from start-col to (+ start-col box-cols -1)
+         (box (loop for r from start-row below (+ start-row box-rows)
+                    append (loop for c from start-col below (+ start-col box-cols)
                                  collect (nth c (nth r (board-cells board)))))))
-    (not (member num (mapcar #'first-elem box)))
-  )
-)
+    (not (member num (mapcar #'cell-first box)))))
 
-;; Verifica se um número é válido considerando as comparações
 (defun isComparativeValid (board num row col)
-  ;; Verifica se o número é válido com base nas comparações
-  (let* ((cell (nth col (nth row board))) ;; Acessa a célula atual
-         (left (cell-second cell)) ;; Acessa a comparação à esquerda
-         (up (cell-third cell)) ;; Acessa a comparação acima
-         (right (cell-fourth cell)) ;; Acessa a comparação à direita
-         (down (cell-fifth cell)) ;; Acessa a comparação abaixo
-         ;; Calcula os valores das células adjacentes
-         (leftVal (if (= col 0) 0 (cell-first (nth (1- col) (nth row board)))))
-         (upVal (if (= row 0) 0 (cell-first (nth col (nth (1- row) board)))))
-         (rightVal (if (= col (- (length (nth 0 board)) 1)) 0 (cell-first (nth (1+ col) (nth row board)))))
-         (downVal (if (= row (- (length board) 1)) 0 (cell-first (nth col (nth (1+ row) board))))))
-    ;; Verifica as comparações em todas as direções
+  ;; Verifica se um número é válido considerando as comparações
+  (format t "num: ~A, row: ~A, col: ~A~%" num row col)
+  (let* ((cell (nth col (nth row (board-cells board))))
+         (left (cell-second cell))
+         (up (cell-third cell))
+         (right (cell-fourth cell))
+         (down (cell-fifth cell))
+         (leftVal (if (= col 0) 0 (cell-first (nth (1- col) (nth row (board-cells board))))))
+         (upVal (if (= row 0) 0 (cell-first (nth col (nth (1- row) (board-cells board))))))
+         (rightVal (if (= col (- (length (nth 0 (board-cells board))) 1)) 0 (cell-first (nth (1+ col) (nth row (board-cells board))))))
+         (downVal (if (= row (- (length (board-cells board)) 1)) 0 (cell-first (nth col (nth (1+ row) (board-cells board)))))))
     (and
-     (or (= col 0) (= leftVal 0) (and (char= left #\<) (< num leftVal)) (and (char= left #\>) (> num leftVal)))
-     (or (= row 0) (= upVal 0) (and (char= up #\<) (< num upVal)) (and (char= up #\>) (> num upVal)))
-     (or (= col (- (length (nth 0 board)) 1)) (= rightVal 0) (and (char= right #\<) (< num rightVal)) (and (char= right #\>) (> num rightVal)))
-     (or (= row (- (length board) 1)) (= downVal 0) (and (char= down #\<) (< num downVal)) (and (char= down #\>) (> num downVal)))
-    )
-  )
+     (or (= col 0) (= leftVal 0) (and (char-equal left #\<) (< num leftVal)) (and (char-equal left #\>) (> num leftVal)))
+     (or (= row 0) (= upVal 0) (and (char-equal up #\<) (< num upVal)) (and (char-equal up #\>) (> num upVal)))
+     (or (= col (- (length (nth 0 (board-cells board))) 1)) (= rightVal 0) (and (char-equal right #\<) (< num rightVal)) (and (char-equal right #\>) (> num rightVal)))
+     (or (= row (- (length (board-cells board)) 1)) (= downVal 0) (and (char-equal down #\<) (< num downVal)) (and (char-equal down #\>) (> num downVal)))))
 )
 
-;; Verifica se um número é válido considerando todas as regras
+
 (defun isValid (board num row col)
+  ;; Verifica se um número é válido considerando todas as regras
   (and (isRowValid board num row)
        (isColValid board num col)
        (isBoxValid board num row col)
-       (isComparativeValid board num row col)
-  )
-)
+       (isComparativeValid board num row col)))
 
-;; Substitui um elemento em uma lista 2D
 (defun replace2D (matrix i j x)
-  (let ((row (nth i matrix)))
-    (setf (nth j row) x)
-    matrix)
-)
+  ;; Substitui um elemento em uma lista 2D
+  (let ((new-row (copy-list (nth i matrix))))
+    (setf (nth j new-row) x)
+    (setf (nth i matrix) new-row)
+    matrix))
 
-;; Encontra uma célula vazia no tabuleiro
 (defun findEmpty (board row col)
-  (cond ((= row (length board)) nil)
-        ((= (first-elem (nth col (nth row board))) 0) (cons row col))
-        ((= col (- (length (nth 0 board)) 1)) (findEmpty board (+ row 1) 0))
-        (t (findEmpty board row (+ col 1)))
-  )
-)
+  ;; Encontra uma célula vazia no tabuleiro
+  (cond ((= row (length (board-cells board))) nil)
+        ((= (cell-first (nth col (nth row (board-cells board)))) 0) (cons row col))
+        ((= col (- (length (nth 0 (board-cells board))) 1)) (findEmpty board (+ row 1) 0))
+        (t (findEmpty board row (+ col 1)))))
 
-;; Tenta preencher uma célula vazia com um número válido
 (defun tryNumber (board num row col)
-  ;; Tentativa de preencher uma célula vazia
-  (cond 
-    ((> num (length board)) nil) ;; Se todos os números foram tentados, retorna nil
-    ((isValid board num row col) ;; Verifica se o número é válido
-     (let* ((currentCell (nth col (nth row board))) ;; Acessa a célula atual
-            (newCell (make-cell :first num ;; Cria uma nova célula com o número e as comparações
-                                :second (cell-second currentCell)
-                                :third (cell-third currentCell)
-                                :fourth (cell-fourth currentCell)
-                                :fifth (cell-fifth currentCell)))
-            (newBoard (replace2D board row col newCell)) ;; Substitui a célula no tabuleiro
-            (nextAttempt (solveComparative newBoard))) ;; Tenta resolver o resto do tabuleiro
-       (if (null nextAttempt)
-           (tryNumber newBoard (+ num 1) row col) ;; Se não for bem-sucedido, tenta o próximo número
-           nextAttempt))) ;; Se bem-sucedido, retorna o tabuleiro resolvido
-    (t (tryNumber board (+ num 1) row col)) ;; Se o número não é válido, tenta o próximo
-  )
-)
+  ;; Tenta preencher uma célula vazia com um número válido
+  (cond ((> num (length (board-cells board))) nil)
+        ((isValid board num row col)
+         (let* ((currentCell (nth col (nth row (board-cells board))))
+                (newCell (make-cell :first num
+                                    :second (cell-second currentCell)
+                                    :third (cell-third currentCell)
+                                    :fourth (cell-fourth currentCell)
+                                    :fifth (cell-fifth currentCell)))
+                (newBoard (make-board :cells (replace2D (board-cells board) row col newCell)))
+                (nextAttempt (solveComparative newBoard)))
+           (if (null nextAttempt)
+               (tryNumber board (+ num 1) row col)
+               nextAttempt)))
+        (t (tryNumber board (+ num 1) row col))))
 
-;; Resolve o tabuleiro
 (defun solveComparative (board)
+  ;; Resolve o tabuleiro
   (if (null (findEmpty board 0 0))
       (list board)
-      (let* (
-            (emptyCell (findEmpty board 0 0))
-            (row (car emptyCell))
-            (col (cdr emptyCell))
-            )
-        (tryNumber board 1 row col)
-      )
+      (let* ((emptyCell (findEmpty board 0 0))
+             (row (car emptyCell))
+             (col (cdr emptyCell)))
+        (tryNumber board 1 row col))))
 
-  )
-)
-
-;; Funções de exibição
 (defun printCell (cell)
-  (let ((value (first cell)))
-    (princ (format nil "~a " value))
-  )
-)
+  ;; Imprime uma célula
+  (princ (format nil "~a " (cell-first cell))))
 
 (defun printBoard (board)
-  (dolist (row board)
+  ;; Imprime o tabuleiro
+  (dolist (row (board-cells board))
     (mapc #'printCell row)
-    (terpri)
-  )
-)
+    (terpri)))
 
-;; Lê o tabuleiro de uma string
 (defun readBoard (input)
-  (read-from-string input)
-)
+  ;; Lê o tabuleiro de uma string
+  (let ((cells (mapcar (lambda (row) 
+                         (mapcar (lambda (cell) 
+                                   (make-cell :first (first cell)
+                                              :second (second cell)
+                                              :third (third cell)
+                                              :fourth (fourth cell)
+                                              :fifth (fifth cell)))
+                                 row))
+                       (read-from-string input))))
+    (make-board :cells cells)))
 
-;; Função principal
 (defun main ()
+  ;; Função principal
   (let ((content (with-open-file (stream "../tabuleiro.txt" :direction :input)
-                   (read-line stream)))
-        )
+                   (read-line stream))))
     (let ((board (readBoard content)))
       (let ((solution (solveComparative board)))
         (if solution
-            (printBoard solution)
-            (princ "No solution found")
-        )
-      )
-    )
-  )
-)
+            (printBoard (first solution))
+            (princ "No solution found"))))))
 
 (main)
